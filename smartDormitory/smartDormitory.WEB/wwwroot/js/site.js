19046806aa210b30e8a4816c4801137a2c411bba
Kacarov <kacarov.martin@gmail.com>
2018-11-26 15:36:27 +0200

    Added Basic RegisterSensor View, RegisterSensor Action, UserSensorViewModel

--- smartDormitory/smartDormitory.WEB/wwwroot/js/site.js
+++ smartDormitory/smartDormitory.WEB/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var myLatlng = new google.maps.LatLng(42.6983, 23.32);
+var myOptions = {
+    zoom: 13,
+    center: myLatlng
+}
+var map = new google.maps.Map(document.getElementById("map"), myOptions);
+var geocoder = new google.maps.Geocoder();
+
+google.maps.event.addListener(map, 'click', function (event) {
+    geocoder.geocode({
+        'latLng': event.latLng
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var splittedLatLang = String(event.latLng).replace('(', '').replace(')', '').split(',');
+
+            alert(splittedLatLang[0])
+            alert(splittedLatLang[1])
+            if (results[0]) {
+                alert(results[0].formatted_address);
+            }
+        }
+    });
+});
